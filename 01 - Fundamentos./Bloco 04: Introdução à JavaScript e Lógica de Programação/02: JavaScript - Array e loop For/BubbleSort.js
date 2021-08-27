f024fe6b48d46cf7b569db96350bc6e3c393f2dd
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-08-27 11:48:13 -0300

    Termina os exercicios do dia 4.2

--- 01 - Fundamentos./Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/BubbleSort.js
+++ 01 - Fundamentos./Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/BubbleSort.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
 // for(var primeiroIndice = 1; primeiroIndice<numbers.length; primeiroIndice+=1){
@@ -24,20 +24,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var multiplicado = [];
+var contador = 1;
 
+for (var index = 0; index < numbers.length; index += 1) {
+    if (numbers[contador] != undefined) {
+        multiplicado.push((numbers[index] * numbers[contador]));
+        contador++
+    } else multiplicado.push((numbers[index] * 2));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log(numbers);
+console.log(multiplicado)

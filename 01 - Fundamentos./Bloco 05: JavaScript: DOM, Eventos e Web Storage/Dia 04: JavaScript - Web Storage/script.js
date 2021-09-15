52fcf26c6897695b7d04aaff3606ddd4a3ef53fc
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-09-15 15:29:33 -0300

    atualiza

--- 01 - Fundamentos./Bloco 05: JavaScript: DOM, Eventos e Web Storage/Dia 04: JavaScript - Web Storage/script.js
+++ 01 - Fundamentos./Bloco 05: JavaScript: DOM, Eventos e Web Storage/Dia 04: JavaScript - Web Storage/script.js
@@ -1,73 +1,95 @@
-function darkMode() {
-  let backgroundColor = 'white';
-  let newColor = 'black';
+window.onload = function() {
+  function darkMode() {
+    let backgroundColor = 'white';
+    let newColor = 'black';
 
-  let botao = document.getElementById('darkMode');
-  botao.addEventListener('click', function() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = localStorage.getItem('bgColor');
 
-    if (document.getElementsByTagName('body')[0].style.backgroundColor === newColor) {
-      document.getElementsByTagName('body')[0].style.backgroundColor = backgroundColor;
+    let botao = document.getElementById('darkMode');
+    botao.addEventListener('click', function() {
 
-    } else {
-      document.getElementsByTagName('body')[0].style.backgroundColor = newColor;
-    }
-  })
+      if (document.getElementsByTagName('body')[0].style.backgroundColor === newColor) {
+        document.getElementsByTagName('body')[0].style.backgroundColor = backgroundColor;
+        localStorage.setItem('bgColor', backgroundColor)
+
+      } else {
+        document.getElementsByTagName('body')[0].style.backgroundColor = newColor;
+        localStorage.setItem('bgColor', newColor)
+      }
+    })
+  }
+  darkMode();
+
+  function colorChanger() {
+    let colors = ['red', 'green', 'blue', 'yellow', 'pink', 'gray']
+
+    let botao = document.getElementById('alteraCor');
+    document.getElementsByTagName('body')[0].style.color = localStorage.getItem('color');
+
+    let index = 0;
+
+    botao.addEventListener('click', function() {
+      document.body.style.color = colors[index];
+      localStorage.setItem('color', colors[index]);
+      localStorage.setItem('count', index);
+      index++;
+
+
+      if (index === 6) {
+        index = 0;
+        // localStorage.setItem('count', 0);
+      }
+    })
+  }
+
+  colorChanger();
+
+
+  let size = document.getElementsByTagName('body')[0].style.fontSize = '1em';
+  console.log(size);
+
+  function sizeChanger() {
+
+    document.getElementsByTagName('body')[0].style.fontSize = localStorage.getItem('size');
+
+    let botao = document.getElementById('alteraTamanho');
+    botao.addEventListener('click', function() {
+
+      if (document.getElementsByTagName('body')[0].style.fontSize === size) {
+        document.getElementsByTagName('body')[0].style.fontSize = '1.3em';
+        localStorage.setItem('size', '1.3em');
+      } else {
+        document.getElementsByTagName('body')[0].style.fontSize = size;
+        localStorage.setItem('size', size);
+      }
+    })
+  }
+  sizeChanger();
+
+  function spaceChanger() {
+    let lineHeigth = document.getElementById('paragrafos').style.lineHeight;
+
+    let botao = document.getElementById('alteraEspacamento');
+    botao.addEventListener('click', function() {
+
+      if (document.getElementById('paragrafos').style.lineHeight === lineHeigth) {
+        document.getElementById('paragrafos').style.lineHeight = '2em';
+      } else document.getElementById('paragrafos').style.lineHeight = lineHeigth;
+
+    })
+  }
+  spaceChanger();
+
+  function familyChanger() {
+    let family = document.getElementById('paragrafos').style.fontFamily;
+
+    let botao = document.getElementById('alteraFonte');
+    botao.addEventListener('click', function() {
+
+      if (document.getElementById('paragrafos').style.fontFamily === family)
+        document.getElementById('paragrafos').style.fontFamily = 'sans';
+      else document.getElementById('paragrafos').style.fontFamily = family;
+    })
+  }
+  familyChanger();
 }
-darkMode();
-
-function colorChanger() {
-  let colors = ['red', 'green', 'blue', 'yellow', 'pink', 'gray']
-
-  let botao = document.getElementById('alteraCor');
-  let index = 0;
-
-  botao.addEventListener('click', function() {
-    document.getElementsByTagName('body')[0].style.color = colors[index];
-    index++
-
-    if (index === 6)
-      index = 0;
-  })
-}
-
-colorChanger();
-
-function sizeChanger() {
-  let size = document.getElementsByTagName('body')[0].style.fontSize;
-
-  let botao = document.getElementById('alteraTamanho');
-  botao.addEventListener('click', function() {
-
-    if (document.getElementsByTagName('body')[0].style.fontSize === size) {
-      document.getElementsByTagName('body')[0].style.fontSize = '1.3em';
-    } else document.getElementsByTagName('body')[0].style.fontSize = size;
-  })
-}
-sizeChanger();
-
-function spaceChanger() {
-  let lineHeigth = document.getElementById('paragrafos').style.lineHeight;
-
-  let botao = document.getElementById('alteraEspacamento');
-  botao.addEventListener('click', function() {
-
-    if (document.getElementById('paragrafos').style.lineHeight === lineHeigth) {
-      document.getElementById('paragrafos').style.lineHeight = '2em';
-    } else document.getElementById('paragrafos').style.lineHeight = lineHeigth;
-
-  })
-}
-spaceChanger();
-
-function familyChanger() {
-  let family = document.getElementById('paragrafos').style.fontFamily;
-
-  let botao = document.getElementById('alteraFonte');
-  botao.addEventListener('click', function() {
-
-    if (document.getElementById('paragrafos').style.fontFamily === family)
-      document.getElementById('paragrafos').style.fontFamily = 'sans';
-    else document.getElementById('paragrafos').style.fontFamily = family;
-  })
-}
-familyChanger();

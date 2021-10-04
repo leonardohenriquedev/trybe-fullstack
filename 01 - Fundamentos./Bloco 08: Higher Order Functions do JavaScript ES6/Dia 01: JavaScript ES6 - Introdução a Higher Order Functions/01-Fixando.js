7fb54550f4adef13ff8124c42e37336f65517396
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-10-04 15:48:54 -0300

    adiciona exercicio de fixacao do dia 8.1

---
+++ 01 - Fundamentos./Bloco 08: Higher Order Functions do JavaScript ES6/Dia 01: JavaScript ES6 - Introdução a Higher Order Functions/01-Fixando.js
@@ -0,0 +1,9 @@
+const goodMorning = () => 'Acordando!!';
+
+const youWant = () => 'Bora tomar café!!';
+
+const youNeed = () => 'Partiu dormir!!';
+
+const doingThings = (func) => func();
+
+console.log(doingThings(action));

8b660590cff25ff2cad38390dbba358290f6ce38
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-10-17 21:50:57 -0300

    Finaliza exercicios do dia 9.1

---
+++ 01 - Fundamentos./Bloco 09: JavaScript e Testes Assíncronos/Dia 01: JavaScript Assíncrono e Callbacks/06-Exercicio.js
@@ -0,0 +1,35 @@
+// 6 - Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature um outro callback que contenha as ações a serem tomadas em caso de falha.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+
+const sendMarsTemperature = (callback, err) => {
+  const temp = getMarsTemperature();
+  const busy = Math.random()*10;
+  if (busy<6) {
+    callback(temp);
+  } else err('Robot is busy');
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
---
+++ 01 - Fundamentos./Bloco 09: JavaScript e Testes Assíncronos/Dia 01: JavaScript Assíncrono e Callbacks/07-Exercicio.test.js
@@ -0,0 +1,18 @@
+// 7 - Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
+
+it('uppercase "test" to equal "TEST"', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});

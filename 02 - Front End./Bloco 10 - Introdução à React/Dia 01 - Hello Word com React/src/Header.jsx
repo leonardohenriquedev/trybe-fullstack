9503cbca016ae572b05745a4665b48838e9f80a8
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-10-28 14:44:03 -0300

    Organiza modulo de Front-End

---
+++ 02 - Front End./Bloco 10 - Introdução à React/Dia 01 - Hello Word com React/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component{
+  render(){
+    return(
+      <>
+        <h1>Conteúdos de Front-End</h1>
+      </>
+    )
+  }
+}
+
+export default Header;

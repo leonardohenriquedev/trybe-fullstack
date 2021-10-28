7a611963fd603f0a99b5b5a0e93e097569a7eb5b
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-10-28 14:35:10 -0300

    Envia exercicios do dia 10.1, primeiro Hello World com React

---
+++ 02 - Front End./Bloco 10 - Introdução à React/Dia 01 - Introdução - Front-end/Conteudos Front-End/conteudos/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.jsx'
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02 - Front End./Bloco 10 - Introdução à React/Dia 01 - Introdução - Front-end/Conteudos Front-End/conteudos/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <h1 className='footer'>E isso é só o começo...</h1>
+    )
+  }
+}
+
+export default Footer;

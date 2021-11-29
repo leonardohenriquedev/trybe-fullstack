fb188e59df61706833b8ad27d566dfcdc294fac2
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-11-29 14:50:35 -0300

    Envia exercicios do dia 14.2

---
+++ 02 - Front End./Bloco 14 - Testes automatizados com React Testing Library/02 - RTL - Mocks e Inputs/Exercicio 1/exercise-one/src/service.js
@@ -0,0 +1,15 @@
+const randomNumberGenerator = () => {
+  return parseInt(Math.random(0, 100) * 100);
+};
+
+function toUpperCase (str) {
+  return str.toUpperCase();
+};
+
+const firstLetter = (str) => {
+  return str[0];
+};
+
+const concat = (str, str2) =>{
+  return str+str2
+}

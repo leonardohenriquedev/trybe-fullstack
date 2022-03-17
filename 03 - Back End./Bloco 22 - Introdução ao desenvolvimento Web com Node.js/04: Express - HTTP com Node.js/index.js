7d91cf983354b1bf1a96f2ecfa711ae5bd334d56
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2022-03-17 13:57:33 -0300

    feat(BackEnd/22.4): Envia exercicios do dia 22.4

---
+++ 03 - Back End./Bloco 22 - Introdução ao desenvolvimento Web com Node.js/04: Express - HTTP com Node.js/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const recipes = [
+  { id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
+  { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
+];
+
+app.get('/recipes', function (req, res) {
+  res.status(200).json(recipes);
+});
+
+app.get('/recipes/pesquisar', function (req, res) {
+  const { name, maxPrice } = req.query;
+  const filteredRecipes = recipes.filter(
+    (r) => r.name.includes(name) && r.preco < parseInt(maxPrice)
+  );
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!' });
+
+  res.status(200).json(recipe);
+});
+
+app.post('/recipes', function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+  res.status(201).json({ message: 'Recipe created successfully!' });
+});
+
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!` });
+});
+
+app.listen(3000, () => {
+  console.log('Aplicação ouvindo na porta 3000');
+});
